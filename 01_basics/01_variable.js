6db73c6d00b5b5044c2fd3de71cbd027fba95bfe
Mohsin Anwar <immohsin0374@gmail.com>
2026-08-16 11:09:48 +0000

    Datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 1372677
+let accountEmail = "hijhja@gmail.com"
+var accountPassword = "672947"
+accountCity = "siwan"
+
+// accountId = 2
+
+console.log(accountId);
+
+accountEmail = "hmsj@gmail.com"
+accountPassword ="7171717"
+accountCity = "harihans"
+
+console.table([accountEmail,accountId ,accountPassword,accountCity])
---
+++ 01_basics/02_Datatype.js
@@ -0,0 +1,25 @@
+"use strict";
+
+console.log(3 + 3);
+
+console.log("mohsin")
+
+let name = "mohsin"
+let age = 21
+let isLogedIn = false
+
+let state;
+
+// number
+// bigint
+// string
+// boolean
+// null
+// undefined
+// symbol
+
+console. log(typeof "mohsin");
+
+console.log(typeof null);
+console.log(typeof unsefined); 
+

367503c4c20ee96a12f8611de3f2ba7181acf784
Mohsin Anwar <immohsin0374@gmail.com>
2026-08-16 12:26:22 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,11 @@
+let score = "33abc"
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = "mohsin"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);

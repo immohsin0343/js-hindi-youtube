bf12a3b4dd041ca6f28b01675386fe83361c1991
Mohsin Anwar <immohsin0374@gmail.com>
2026-08-19 11:24:38 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// Primitive
+
+// 7 types String, Number, Boolearn, null, undefined, Symbol,bigint
+
+
+ const score = 100
+ const scoreValue = 100.3
+
+
+ const isLoggedIn = false
+ const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+console.log(id === anotherId);
+
+// const bigNumber = 52828529233758236n
+
+//  refrence (non primitive)
+
+// array, objects, function
+
+
+const heroes = ["shaktiman", "nagraj", "doga"]
+
+ let myObj = {
+
+    name: "mohsin",
+
+    age: 22
+}
+
+const myFunction =function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof outsideTemp);

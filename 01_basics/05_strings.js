ee148a008d0ba58001dcf517b5f198e99eb6b2fe
Mohsin Anwar <immohsin0374@gmail.com>
2026-08-20 13:34:55 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,20 @@
+const name = "Mohsin"
+const repoCount = 50 
+
+// console.log(name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo Count is ${repoCount}`);
+
+
+const gameName = new String('Mohsin')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+
+
